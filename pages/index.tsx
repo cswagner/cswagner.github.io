6674a6908697b5b69b7beeffb52f1db71a3ac55c
Chris Wagner <mail@cswagner.io>
2024-03-09 18:16:56 -0500

    Add name and title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
-const HomePage = () => null
+const HomePage = () => (
+  <div className="flex h-screen items-center justify-center">
+    <div className="flex flex-col items-center sm:items-end">
+      <div className="text-center text-4xl text-neutral-100">Chris Wagner</div>
+      <div className="text-center font-thin text-neutral-500">
+        Full-Stack Software Engineer
+      </div>
+    </div>
+  </div>
+)
 
 export default HomePage
