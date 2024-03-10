d3313719cb700dd013ccd4f8acd2fdf25450c829
Chris Wagner <mail@cswagner.io>
2024-03-10 17:27:05 -0400

    Add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import {
   FontAwesomeIcon,
   FontAwesomeIconProps,
@@ -6,29 +7,36 @@ import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const HomePage = () => (
-  <div className="flex h-screen flex-col items-center justify-center max-sm:space-y-4 sm:flex-row sm:space-x-4">
-    <div className="flex flex-col items-center sm:items-end">
-      <div className="text-center text-4xl text-neutral-100">Chris Wagner</div>
-      <div className="text-center font-thin text-neutral-500">
-        Full-Stack Software Engineer
+  <div>
+    <Head>
+      <title>Chris Wagner</title>
+    </Head>
+    <div className="flex h-screen flex-col items-center justify-center max-sm:space-y-4 sm:flex-row sm:space-x-4">
+      <div className="flex flex-col items-center sm:items-end">
+        <div className="text-center text-4xl text-neutral-100">
+          Chris Wagner
+        </div>
+        <div className="text-center font-thin text-neutral-500">
+          Full-Stack Software Engineer
+        </div>
       </div>
-    </div>
-    <div className="flex flex-row items-center space-x-4">
-      <IconLink
-        icon={faGithub}
-        className="hover:text-rose-500 active:text-rose-500"
-        url="https://www.github.com/cswagner"
-      />
-      <IconLink
-        icon={faLinkedinIn}
-        className="hover:text-blue-500 active:text-blue-500"
-        url="https://www.linkedin.com/in/cswagner"
-      />
-      <IconLink
-        icon={faEnvelope}
-        className="hover:text-emerald-500 active:text-emerald-500"
-        url="mailto:contact@cswagner.io"
-      />
+      <div className="flex flex-row items-center space-x-4">
+        <IconLink
+          icon={faGithub}
+          className="hover:text-rose-500 active:text-rose-500"
+          url="https://www.github.com/cswagner"
+        />
+        <IconLink
+          icon={faLinkedinIn}
+          className="hover:text-blue-500 active:text-blue-500"
+          url="https://www.linkedin.com/in/cswagner"
+        />
+        <IconLink
+          icon={faEnvelope}
+          className="hover:text-emerald-500 active:text-emerald-500"
+          url="mailto:contact@cswagner.io"
+        />
+      </div>
     </div>
   </div>
 )
