1df65524df6e2c1f26cc0d25a5f80b58932ce6f8
Chris Wagner <mail@cswagner.io>
2025-03-29 16:48:22 -0400

    Scaffold blog layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,34 +15,43 @@ const HomePage = () => (
         content="Profile for Chris Wagner, full-stack software engineer"
       />
     </Head>
-    <div className="flex h-screen flex-col items-center justify-center max-sm:space-y-4 sm:flex-row sm:space-x-4">
-      <div className="flex flex-col items-center sm:items-end">
-        <div className="text-center text-4xl text-neutral-100">
-          Chris Wagner
-        </div>
-        <div className="text-center font-thin text-neutral-400">
-          Full-Stack Software Engineer
+    <div className="flex h-screen flex-col lg:flex-row">
+      <div className="flex justify-center p-8 lg:items-center">
+        <div className="flex flex-col items-center space-y-4">
+          <div className="flex flex-col">
+            <div className="text-center text-4xl text-neutral-100">
+              Chris Wagner
+            </div>
+            <div className="text-center font-thin text-neutral-400">
+              Full-Stack Software Engineer
+            </div>
+          </div>
+          <div className="flex flex-row items-center space-x-4">
+            <IconLink
+              icon={faGithub}
+              className="hover:text-rose-500 active:text-rose-500"
+              url="https://www.github.com/cswagner"
+              ariaLabel="GitHub"
+            />
+            <IconLink
+              icon={faLinkedinIn}
+              className="hover:text-blue-500 active:text-blue-500"
+              url="https://www.linkedin.com/in/cswagner"
+              ariaLabel="LinkedIn"
+            />
+            <IconLink
+              icon={faEnvelope}
+              className="hover:text-emerald-500 active:text-emerald-500"
+              url="mailto:contact@cswagner.io"
+              ariaLabel="Email"
+            />
+          </div>
         </div>
       </div>
-      <div className="flex flex-row items-center space-x-4">
-        <IconLink
-          icon={faGithub}
-          className="hover:text-rose-500 active:text-rose-500"
-          url="https://www.github.com/cswagner"
-          ariaLabel="GitHub"
-        />
-        <IconLink
-          icon={faLinkedinIn}
-          className="hover:text-blue-500 active:text-blue-500"
-          url="https://www.linkedin.com/in/cswagner"
-          ariaLabel="LinkedIn"
-        />
-        <IconLink
-          icon={faEnvelope}
-          className="hover:text-emerald-500 active:text-emerald-500"
-          url="mailto:contact@cswagner.io"
-          ariaLabel="Email"
-        />
+      <div className="lg:px0 flex flex-1 flex-col gap-4 overflow-y-scroll px-8 lg:px-0 lg:pe-8">
+        <div className="hidden min-h-4 flex-1 lg:flex" />
+        {/* TODO: Add blog post here */}
+        <div className="flex min-h-4 flex-1" />
       </div>
     </div>
   </div>
